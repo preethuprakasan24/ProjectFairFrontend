304f3ae93e691bae4415d7afa010b3a077825028
preethuprakasan24 <preethuprakasan34@gmail.com>
2024-08-27 13:55:21 +0530

    contextchange

--- src/context/Contextshare.jsx
+++ src/context/Contextshare.jsx
@@ -2,14 +2,14 @@ import React, { createContext, useState } from "react";
 
 export const addResponseContext = createContext({});
 export const editResponseContext = createContext({});
-export const isLoginAuthContext = createContext(false);
+export const isLoginAuthContext = createContext(true);
 
 function Contextshare({ children }) {
   //children is a predefined prop to share data
 
   const [addResponse, setAddResponse] = useState({});
   const [editResponse, setEditResponse] = useState({});
-  const [isLoginStatus, setIsLoginStatus] = useState(false);
+  const [isLoginStatus, setIsLoginStatus] = useState(true);
   return (
     <>
       <addResponseContext.Provider value={{ addResponse, setAddResponse }}>
